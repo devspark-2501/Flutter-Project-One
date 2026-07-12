82e5df6fe57fa684144b4ddc3cddc13f9b74eed2
Tanush Mathur <devspark2501@gmail.com>
2026-07-12 07:51:52 +0530

    OOP completed

--- lib/OOP.dart
+++ lib/OOP.dart
@@ -1,5 +1,45 @@
 import 'dart:io';
 
-void main() {
+class User {
+  String name;
+  int age;
 
+  User({
+    required this.name,
+    required this.age,
+  });
 }
+
+void main() {
+  User u1 = User(
+    name: 'Tanush',
+    age: 16,
+  );
+
+  print(u1.name); // Tanush
+  print(u1.age);  // 16
+}
+
+// import 'dart:io';
+//
+// class User {
+//   String name;
+//   int age;
+//
+//   User({
+//     required this.name,
+//     required this.age),
+//   }
+// }
+//
+//
+// void main() {
+//   User u1 = User(
+//       name: 'Tanush',
+//       age: 16
+//   ); // instance
+//
+//   // User u2 = User();
+//
+//   print(u1);
+// }
