f0c739f5e62853760af34f1a3cd5d8f51e928365
Tanush Mathur <devspark2501@gmail.com>
2026-07-22 17:17:46 +0530

    Starter Code!!

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MyApp()
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp();
+    return MaterialApp(
+      home: HomeScreen()
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My first App')
+      ),
+    );
   }
 }
