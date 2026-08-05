343fbc4ef2ebfd79358656e5d01942faeaa7897b
Tanush Mathur <devspark2501@gmail.com>
2026-08-05 22:13:33 +0530

    Navigation and routing done

---
+++ lib/flutterNav.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    routes: {
+      '/': (context) => HomeScreen(),
+      '/second': (context) => SecondScreen(),
+    },
+  ));
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/second');
+          },
+          child: Text("Go to Second"),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Second')),
+      body: Center(child: Text("Second Page")),
+    );
+  }
+}
--- lib/flutterstate.dart
+++ lib/flutterstate.dart
@@ -102,3 +102,5 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+// navigation method
